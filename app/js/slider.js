35e9b645763ff023503653a5a1857b747a9c8064
zaxanq <zaxanq@gmail.com>
2019-05-09 10:44:27 +0200

    Sliders exchanged to buttons. Added functionality to the buttons.

--- app/js/slider.js
+++ app/js/slider.js
@@ -1,34 +1,80 @@
 
 class Slider {
     init() {
-        this.taskSlider = document.getElementById('task-slider');
-        this.breakSlider = document.getElementById('break-slider');
+        this.initialSessionValue = 25;
+        this.initialBreakValue = 5;
+        this.sessionValue = this.initialSessionValue;
+        this.breakValue = this.initialBreakValue;
 
-        this.taskSliderValueContainer = document.getElementsByClassName('task-slider-value')[0];
-        this.breakSliderValueContainer = document.getElementsByClassName('break-slider-value')[0];
+        this.sessionClass = 'settings__session';
+        this.session = document.getElementsByClassName(this.sessionClass)[0];
+        this.breakClass = 'settings__break';
+        this.break = document.getElementsByClassName(this.breakClass)[0];
 
-        this.renderValues('both');
+        this.sessionValueContainer = document.getElementsByClassName('session-value')[0];
+        this.breakValueContainer = document.getElementsByClassName('break-value')[0];
 
-        this.taskSlider.addEventListener('change', () => {
-            this.renderValues('task');
-        });
+        this.renderValues('initial');
 
-        this.breakSlider.addEventListener('change', () => {
-            this.renderValues('break');
-        });
+        this.addOnClick(this.session);
+        this.addOnClick(this.break);
+
     }
 
-    renderValues(type) {
-        if (type === 'both') {
-            this.taskSliderValueContainer.innerText = this.taskSlider.value;
-            this.breakSliderValueContainer.innerText = this.breakSlider.value;
-        } else  if (type === 'task') {
-            this.taskSliderValueContainer.innerText = this.taskSlider.value;
-        } else if (type === 'break') {
-            this.breakSliderValueContainer.innerText = this.breakSlider.value;
+    renderValues(valueType) {
+        if (valueType === 'session') {
+            this.sessionValueContainer.innerText = this.sessionValue;
+        } else if (valueType === 'break') {
+            this.breakValueContainer.innerText = this.breakValue;
+        } else if (valueType === 'initial') {
+            this.sessionValueContainer.innerText = this.initialSessionValue;
+            this.breakValueContainer.innerText = this.initialBreakValue;
         } else {
-            console.log('ERROR: Undefined value to render.');
+            console.log('ERROR: Undefined value container.');
         }
+    }
+
+    updateValue(type, changeValue) {
+        let value;
+        if (type === 'session') {
+            value = this.sessionValue;
+        } else if (type === 'break') {
+            value = this.breakValue;
+        } else {
+            console.log('ERROR: No value specified to update.');
+        }
+
+        if (value < -changeValue) {
+            value = 0;
+        } else if (value + changeValue > 60) {
+            value = 60;
+            //TODO: add info that it's not possible to add more
+        } else {
+            value += changeValue;
+        }
+
+        return value;
+    }
+
+    addOnClick(element) {
+        this.addButtonClick(element, 'increase', 1);
+        this.addButtonClick(element, 'increase', 10);
+        this.addButtonClick(element, 'decrease', -1);
+        this.addButtonClick(element, 'decrease', -10);
+    }
+
+    addButtonClick(element, type, changeValue) {
+        document.querySelector(`.${element.classList[0]} .${type}-${Math.abs(changeValue)}`).addEventListener('click', () => {
+            if (element.classList[0] === this.sessionClass) {
+                this.sessionValue = this.updateValue('session', changeValue);
+                this.renderValues('session')
+            } else if (element.classList[0] === this.breakClass) {
+                this.breakValue = this.updateValue('break', changeValue);
+                this.renderValues('break');
+            } else {
+                console.log('ERROR: No buttons found to add EventListener.')
+            }
+        });
     }
 
     toInput() {
